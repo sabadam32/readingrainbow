55ab4e07bcde1b213e15e5b6a2ca31d7329a48bc
Rich Saupe <rich.saupe@gmail.com>
2024-07-20 21:29:15 -0500

    added favicon and number event

--- app/views/mds.py
+++ app/views/mds.py
@@ -2,6 +2,8 @@ from pyramid.view import view_config
 from pyramid.response import Response
 from sqlalchemy.exc import SQLAlchemyError
 
+from app.models.mymodel import Category
+
 from .. import models
 
 @view_config(route_name='mds', renderer='app:templates/mds.jinja2')
@@ -13,18 +15,31 @@ def my_view(request):
     for level in data['active']:
         index = len(level)-1
         try:
-            data[levels[index]] = query.filter(models.Category.mds_number.like(f'{level[:-1]}_')).all()
+            data[levels[index]] = query.filter(
+                models.Category.mds_number.like(f'{level[:-1]}_')
+                ).order_by(Category.mds_number).all()
         except SQLAlchemyError:
             return Response('Nogood', content_type='text/plain', status='500')
     else:
         res = max(data['active'], key=len)
         if len(res) < 6:
             if len(res) == 3:
-                data['tenths'] = query.filter(models.Category.mds_number.like(f'{res}._')).all()
+                data['tenths'] = query.filter(
+                    models.Category.mds_number.like(f'{res}._')
+                    ).order_by(Category.mds_number).all()
             else:
                 data[levels[len(res)]] = query.filter(models.Category.mds_number.like(f'{res}_')).all()
     return data
 
+def get_row(session, number):
+    if len(number) == 3:
+        next_char = '._'
+    else:
+        next_char = '_'
+
+    session.filter(
+        models.Category.mds_number.like(f'{number}{next_char}')
+                    ).order_by(Category.mds_number).all()
 
 def get_rows(number):
     levels = ['hundreds', 'tens', 'ones', '', 'tenths', 'hundredths']
