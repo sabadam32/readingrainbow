99309a14c74a874e66485ef28586d705e891452a
sabadam32 <sabadam32@gmail.com>
2024-08-01 04:53:28 -0500

    add execution scripts

---
+++ install.bat
@@ -0,0 +1,2 @@
+python python\pip.pyz install -r requires.txt --no-index --find-links=packages
+python python\pip.pyz install -e .
---
+++ run.cmd
@@ -0,0 +1,3 @@
+call python -m webbrowser -n http://localhost
+call pserve development.ini
+
